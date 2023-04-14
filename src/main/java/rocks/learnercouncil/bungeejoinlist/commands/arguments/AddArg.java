86407f12cf7e17d4cb0528b932acaf7e4d6095a6
H2ofiremaster <h2ofiremaster@gmail.com>
2023-04-14 12:34:12 -0700

    Merged 'RequestHandler' into `AddArg`

--- src/main/java/rocks/learnercouncil/bungeejoinlist/commands/arguments/AddArg.java
+++ src/main/java/rocks/learnercouncil/bungeejoinlist/commands/arguments/AddArg.java
@@ -1,17 +1,25 @@
 package rocks.learnercouncil.bungeejoinlist.commands.arguments;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.BaseComponent;
-import rocks.learnercouncil.bungeejoinlist.RequestHandler;
+import rocks.learnercouncil.bungeejoinlist.BungeeJoinlist;
 import rocks.learnercouncil.bungeejoinlist.commands.CommandArgument;
 import rocks.learnercouncil.bungeejoinlist.commands.CommandResult;
 import rocks.learnercouncil.bungeejoinlist.data.PlayerData;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class AddArg implements CommandArgument {
+    private static final BungeeJoinlist plugin = BungeeJoinlist.getPlugin();
+
     @Override
     public BaseComponent[] execute(CommandSender sender, String[] args) {
         if(!args[0].equalsIgnoreCase("add")) return CommandResult.NONE;
@@ -19,10 +27,28 @@ public class AddArg implements CommandArgument {
         if(args.length > 2) return CommandResult.TOO_MANY_ARGS;
 
         String username = args[1];
-        Optional<PlayerData> playerData = RequestHandler.requestUUID(username);
+        Optional<PlayerData> playerData = requestUUID(username);
         if(!playerData.isPresent()) return CommandResult.notFound(username);
         playerData.get().add();
         return CommandResult.added(playerData.get().getName());
+    }
+
+    public Optional<PlayerData> requestUUID(String username) {
+        try {
+            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
+            InputStreamReader inputStreamReader = new InputStreamReader(url.openStream());
+            BufferedReader in = new BufferedReader(inputStreamReader);
+
+            JsonObject jsonObject = (JsonObject) JsonParser.parseReader(in);
+            String uuid = jsonObject.get("id").getAsString().replaceAll("(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5");
+            String name = jsonObject.get("name").getAsString();
+            in.close();
+
+            return Optional.of(new PlayerData(name, UUID.fromString(uuid)));
+        } catch (Exception e) {
+            plugin.getLogger().severe("Could net get UUID of " + username);
+            return Optional.empty();
+        }
     }
 
     @Override
